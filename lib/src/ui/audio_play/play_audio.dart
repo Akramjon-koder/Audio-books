125422d84e059a1028977378220e8493c8de8631
Akramjon <akramjonminochdinov@gmail.com>
2024-12-06 15:07:36 +0500

    first commit

--- lib/src/ui/audio_play/play_audio.dart
+++ lib/src/ui/audio_play/play_audio.dart
@@ -14,6 +14,7 @@ class AudioPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Screen(
       title: 'Audio',
+
       child: Column(
         children: [
           Expanded(
